264f378d05012c2a816c613b77b4c48168c386ef
Roman-Koval <kovalrv83@gmail.com>
2021-11-30 18:55:45 +0200

    вторая готова

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
 
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (date) {
+    localStorage.setItem('videoplayer-current-time', date.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
